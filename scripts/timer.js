83ae9f384601e88fae0eb3261134dc75b5ecc8b4
danielesechiii <danielesechi.dev@gmail.com>
2026-09-19 20:49:57 +0200

    timer.js

--- scripts/timer.js
+++ scripts/timer.js
@@ -7,9 +7,25 @@ function startTimer() {
     endTime = Date.now() + remaining;
 };
 
+function tick() {
+    if (state !== 'RUNNING') return null;
+
+    remaining = endTime - Date.now();
+
+    if (remaining <= 0) {
+        state = 'FINISH';
+        remaining = 0;
+    }
+        
+    return { state, remaining };
+}
+
 function reset() {
     state = 'IDLE'
     endTime = 0;
     remaining = 1_500_000;
     return;
 };
+
+export { startTimer, tick, reset };
+
